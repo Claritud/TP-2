687bfd03ef3e4ad0b2540866b3ce2a64bdd8bbd8
ClaraRubio <mclararubio@gmail.com>
2019-06-30 19:00:15 -0300

    funcionando 2 y 6

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+// local.ventas[ i ].componentes[ i ]
+
 //1.1 precioMaquina(componentes): recibe un array de componentes y devuelve el precio de la máquina que se puede armar con esos componentes, que es la suma de los precios de cada componente incluido.
 
 // const precioMaquina = (...cosas) => {
 //     let suma = 0;  
 //     for (const cosa of cosas){
-//         for (const component of local.precios){        
-//             if (component.componente === cosa){
-//                 suma += component.precio;
+//             let sumaParcial = 0;
+//         for (const component of local.precios){  
+//                sumaParcial += suma;      
+//             if (component.componente === cosa[0]){
+//                 suma = component.precio;
 //             }           
 //         }  
 //     }    
@@ -13,33 +17,43 @@
 // }
 // console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]) ); // 320 ($200 del monitor + $120 del motherboard)
 
-
-const precioMaquina = (...cosas) => {
-    let losQueSi = [];
-    for (const cosa of cosas){
-     const componenteABuscar = component => component.componente === cosa;
-     losQueSi = local.precios.filter(componenteABuscar)
-     
-    } 
-    const sumar =(total, price) => total += price;
-    return losQueSi.precio.reduce(sumar); // aca podria poner .precio antes de reduce y sacar el de arriba despues de el filter
+const precioMaquina = (...components) => {
+    
+    const estaComponente = venta => venta.componentes.includes(component);
+    return local.ventas.filter(estaComponente).price;
 }
-console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]) ); // 320 ($200 del monitor + $120 del motherboard)
 
+// const precioMaquina = (...cosas) => {
+//     let losQueSi = [];
+//     for (const cosa of cosas){
+//      const componenteABuscar = component => component.componente === cosa[0];
+//      losQueSi = local.precios.filter(componenteABuscar);
+     
+//     } 
+//     const sumar =(total, price) => total += price;
+//     return losQueSi.precio.reduce(sumar); // aca podria poner .precio antes de reduce y sacar el de arriba despues de el filter
+// }
+// console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]) ); // 320 ($200 del monitor + $120 del motherboard)
+
+// const precioMaquina = (...cosas) => {
+//     let losQueSi;
+//     for (let i = 0; i < cosas.length; i++){
+//      const componenteABuscar = component => component.componente === cosas[i];
+//      losQueSi = local.precios.filter(componenteABuscar).precio;   
+//     } 
+//     const sumar =(total, price) => total += price;
+//     return losQueSi.reduce(sumar); // aca podria poner .precio antes de reduce y sacar el de arriba despues de el filter
+// }
+// console.log(precioMaquina(["Monitor GPRS 3000", "Motherboard ASUS 1500"]) ); // 320 ($200 del monitor + $120 del motherboard)
 
 //1.2 cantidadVentasComponente(componente): recibe un componente y devuelve la cantidad de veces que fue vendido, o sea que formó parte de una máquina que se vendió. La lista de ventas no se pasa por parámetro, se asume que está identificada por la variable ventas.
 
-const cantidadVentasComponente = (component) => {
-
-    const componenteVendido = venta => venta.componentes[0] === component;
-    return local.ventas.filter(componenteVendido).length;
+const cantidadVentasComponente = component => {
+    
+    const estaComponente = venta => venta.componentes.includes(component);
+    return local.ventas.filter(estaComponente).length;
 }
-
-console.log( cantidadVentasComponente("Monitor ASC 543") ); // 2
-
-//aca falta que recorra el array de componentes porque si no tieen el [0] no funca y asi lo lee pero solo el ejemplo esta en la posicion 0;
-
-
+console.log(cantidadVentasComponente("Monitor ASC 543")); // 2
 
 // 1.3 vendedoraDelMes(mes, anio), se le pasa dos parámetros numéricos, (mes, anio) y devuelve el nombre de la vendedora que más vendió en plata en el mes. O sea no cantidad de ventas, sino importe total de las ventas. El importe de una venta es el que indica la función precioMaquina. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre).
 
@@ -50,3 +64,34 @@ console.log( cantidadVentasComponente("Monitor ASC 543") ); // 2
 // console.log( vendedoraDelMes(1, 2019) ); // "Ada" (vendio por $670, una máquina de $320 y otra de $350)
 
 
+
+//1.4 ventasMes(mes, anio): Obtener las ventas de un mes. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre).
+
+// console.log( ventasMes(1, 2019) ); // 1250
+
+//1.5 ventasVendedora(nombre): Obtener las ventas totales realizadas por una vendedora sin límite de fecha.
+
+const ventasVendedora = name => {
+
+}
+console.log( ventasVendedora("Grace") ); // 900
+
+//1.6 componenteMasVendido(): Devuelve el nombre del componente que más ventas tuvo historicamente. El dato de la cantidad de ventas es el que indica la función cantidadVentasComponente
+
+const componenteMasVendido = () => {
+    let mayorVenta = '';
+    let anterior = 0;
+    for (let i = 0; i < local.precios.length; i++) {     
+     let ventasComponente = cantidadVentasComponente(local.precios[i].componente);
+        if (anterior < ventasComponente) {
+            anterior = ventasComponente;
+            mayorVenta = local.precios[i].componente;
+        }       
+    }
+    return mayorVenta;   
+}
+console.log( componenteMasVendido() ); // Monitor GPRS 3000
+
+//1.7 huboVentas(mes, anio): que indica si hubo ventas en un mes determinado. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre).
+
+// console.log( huboVentas(3, 2019) ); // false
